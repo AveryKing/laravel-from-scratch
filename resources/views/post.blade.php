eb62add9fa8cf424e53c5d9d9f4cce83c161f4d2
Avery King <kavery086@gmail.com>
2022-05-01 03:18:15 -0400

    post extracted to separate view

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <title>Laravel</title>
+    <link rel="stylesheet" type="text/css" href="/app.css"/>
+
+</head>
+<body>
+
+
+<article>
+    <?= $title ?>
+    <?= $body ?>
+</article>
+<a href="/">Go back</a>
+<script type="text/javascript" src="/app.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+});
+
+Route::get('post', function () {
+    return view('post', [
+        'title' => '<h1>Post Title</h1>',
+        'body' => '<p>Post Content</p>',
+    ]);
 });
