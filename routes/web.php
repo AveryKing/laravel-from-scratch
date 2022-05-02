f318b136fbac3a29c015f992f94db57f3d5ef5cc
Avery King <kavery086@gmail.com>
2022-05-02 02:03:41 -0400

    refactor

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts');
-});
+Route::get('/', fn() => view('posts'));
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ .'/../resources/posts/'.$slug .'.html';
-    if(!file_exists($path)) {
-        //return redirect('/');
+    $path = __DIR__ . '/../resources/posts/' . $slug . '.html';
+    if (!file_exists($path)) {
         abort(404);
     }
 
-    $post = cache()->remember("post.{$slug}", 5, function() use ($path) {
-        var_dump('file_get_contents');
-        return file_get_contents($path);
-    });
-
+    $post = cache()->remember("post.{$slug}", 5, fn() => file_get_contents($path));
 
     return view('post', [
         'post' => $post
